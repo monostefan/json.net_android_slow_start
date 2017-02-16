745767846c5a02884c4a43f878f1fd7a1f1c2c62
Stefan Reinhard <stefan@monostream.com>
2017-02-16 15:46:13 +0100

    Initial commit

---
+++ JsonNetSlowStart/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Diagnostics;
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace JsonNetSlowStart
+{
+    [Activity(Label = "Json.Net slow start", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button parseJsonButton;
+        TextView resultText;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            parseJsonButton = FindViewById<Button>(Resource.Id.parseJsonButton);
+            resultText = FindViewById<TextView>(Resource.Id.resultText);
+            parseJsonButton.Click += ParseJson;
+        }
+
+        private async void ParseJson(object sender, System.EventArgs e)
+        {
+            resultText.Text = "Parsing...";
+            var stopWatch = Stopwatch.StartNew();
+
+            await Task.Run(() =>
+            {
+                using (var stream = Assets.Open("people.json"))
+                using (var streamReader = new StreamReader(stream))
+                {
+                    var json = streamReader.ReadToEnd();
+                    var people = JsonConvert.DeserializeObject<Person[]>(json);
+                    foreach (var person in people)
+                    {
+                        System.Diagnostics.Debug.WriteLine(person.Name);
+                    }
+                }
+            });
+
+            stopWatch.Stop();
+            resultText.Text = $"Took {stopWatch.ElapsedMilliseconds}ms";
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,10 @@
+
+# JsonNetSlowStart
+
+A small repo case to demonstrate slow first parsing with Newtonsoft Json.NET on Xamarin.Android.
+
+My average values on different devices:
+
+First time: More than 800 ms
+
+Second time: Less than 20 ms
